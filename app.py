53217bcc770f33d88425181ba42bbbee6ede52b7
Scott Wears <scottwears1@outlook.com>
2021-09-29 08:11:07 +0100

    A lot of stuff

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
@@ -15,13 +15,113 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+reviews = list(mongo.db.reviews.find())
+games = list(mongo.db.games.find())
 
 
+@app.route("/home")
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-   
-    return "Hello World"
+def home():
+
+    return render_template("reviews.html", reviews=reviews)
+
+
+@ app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("home"))
+
+    return render_template("register.html")
+
+
+@ app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@ app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    if request.method == "POST":
+        reviews = {
+            "game_name": request.form.get("game_name"),
+            "review_description": request.form.get("review_description"),
+            "created_by": session["user"]
+        }
+        mongo.db.reviews.insert_one(reviews)
+        return redirect(url_for("home"))
+
+    games = mongo.db.games.find().sort("games", 1)
+    return render_template("add_reviews.html", games=games)
+
+
+@ app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username, reviews=reviews)
+
+    return redirect(url_for("login"))
+
+
+@ app.route("/logout")
+def logout():
+    # remove user from session cookie
+
+    session.pop("user")
+    return redirect(url_for("home"))
+
+# Handle errors
+
+
+# @app.errorhandler(Exception)
+# def page_not_found(e):
+#     # note that we set the 404 status explicitly
+#     return render_template('404.html', error=e), 404
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
